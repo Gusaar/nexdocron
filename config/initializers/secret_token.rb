e1ef490f021997003e2136adb35f76653a39792b
Gusaar <sir.gusaar.hiryoken@gmail.com>
2013-04-15 18:42:12 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nexdocron::Application.config.secret_token = '004c7fbcde84d60d50771a7e0ed729a6028426281613aab58e542c357988de16851089f66edd9e5154e9d243b53e966effee89845260e4899c3ebdddc18fa0cb'
