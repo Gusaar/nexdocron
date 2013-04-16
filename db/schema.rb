5241a3966ed953586e4e8fb5ce4529ed7c0b3356
Diego Gaitán <diego.gaitan@gmail.com>
2013-04-16 13:55:41 -0500

    Initialized rSpec

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,34 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130416181655) do
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                                :default => "", :null => false
+    t.string   "encrypted_password",                                   :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                                        :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "skype_id"
+    t.string   "phone"
+    t.string   "mobile"
+    t.decimal  "rate",                   :precision => 8, :scale => 2
+    t.datetime "created_at",                                                           :null => false
+    t.datetime "updated_at",                                                           :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["first_name"], :name => "index_users_on_first_name"
+  add_index "users", ["last_name"], :name => "index_users_on_last_name"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
