f8ea82bdb955c5d956fcdc51b3428d13cbf77f95
Gusaar <sir.gusaar.hiryoken@gmail.com>
2013-04-17 10:30:26 -0500

    Some devise adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Nexdocron::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+
+  resources :dashboards, :only => [ :index ]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +52,7 @@ Nexdocron::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dashboards#index'
 
   # See how all your routes lay out with "rake routes"
 
